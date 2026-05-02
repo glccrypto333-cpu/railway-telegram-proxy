3a42d20246fdc8d62bda6b7f4892805738680a84
glccrypto333-cpu <glccrypto333@gmail.com>
2026-05-02 13:06:36 +0300

    add microsocks auth debug logs

--- start.sh
+++ start.sh
@@ -4,4 +4,8 @@ set -e
 : "${PROXY_USER:=telegram}"
 : "${PROXY_PASS:?PROXY_PASS is required}"
 
+echo "Starting microsocks on 0.0.0.0:1080"
+echo "SOCKS5 user: $PROXY_USER"
+echo "Password length: ${#PROXY_PASS}"
+
 exec microsocks -i 0.0.0.0 -p 1080 -u "$PROXY_USER" -P "$PROXY_PASS"
